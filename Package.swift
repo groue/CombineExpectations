c48ca888e2899e3874e6104925ea19c5c80a9a28
Chris Araman <chris@hiddenplace.dev>
2021-06-18 12:30:29 -0700

    Remove empty dependencies declaration

--- Package.swift
+++ Package.swift
@@ -16,10 +16,6 @@ let package = Package(
             name: "CombineExpectations",
             targets: ["CombineExpectations"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
